80393b75f6794d50c7ddc189a4f25efeb37af39c
Juliana <juliana.fulanetto@aluno.senai.br>
2025-03-25 09:01:17 -0300

    alterado navbar

---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const NavBar = (props) => {
+  return (
+    <div className="bg-black-gradient d-flex justify-content-between align-items-center w-100 p-5 pt-0">
+      <img className="img-fluid col-2" src={props.Logo} alt="Logo" />
+
+      <div className="search col-7">
+        <input
+          onKeyDown={props.handleKeyPress}
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="Pesquise por filmes"
+        />
+
+        <img onClick={() => searchMovies(props.search)} src={props.Lupa} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
