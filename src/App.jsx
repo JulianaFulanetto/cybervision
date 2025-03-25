58d2f2079990f019a0d31332568ed85cf3803207
Juliana <juliana.fulanetto@aluno.senai.br>
2025-03-25 10:35:42 -0300

    Alterando o Nav-Bar

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Footer from "./components/footer/Footer";
 import MovieCards from "./components/movieCard/MovieCards";
 import Lupa from "./assets/search.svg";
 import Logo from "./assets/Logo.png";
-// import NavBar from "./components/navBar/NavBar";
+import NavBar from "./components/navBar/NavBar";
 
 // import Logo from ""
 
@@ -25,7 +25,6 @@ const App = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${apiUrl}&s=${title}`);
     const data = await response.json();
-
     //alimentando o movies
     setMovies(data.Search);
   };
@@ -37,21 +36,16 @@ const App = () => {
 
   return (
     <div id="app">
-      {/* <NavBar /> */}
-      <div className="bg-black-gradient d-flex justify-content-between align-items-center w-100 p-5 pt-0">
-        <img className="img-fluid col-2" src={Logo} alt="Logo" />
 
-        <div className="search col-7">
-          <input
-            onKeyDown={handleKeyPress}
-            onChange={(e) => setSearch(e.target.value)}
-            type="text"
-            placeholder="Pesquise por filmes"
-          />
-
-          <img onClick={() => searchMovies(search)} src={Lupa} alt="" />
-        </div>
-      </div>
+      <NavBar 
+      Logo={Logo}
+      Lupa={Lupa}
+      setSearch={setSearch}
+      search={search}
+      searchMovies={searchMovies}
+      handleKeyPress={handleKeyPress}
+    />
+      
       {movies?.length > 0 ? (
         <div className="container">
           {movies.map((movie, index) => (
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const NavBar = (props) => {
   return (
     <div className="bg-black-gradient d-flex justify-content-between align-items-center w-100 p-5 pt-0">
@@ -8,12 +9,12 @@ const NavBar = (props) => {
       <div className="search col-7">
         <input
           onKeyDown={props.handleKeyPress}
-          onChange={(e) => setSearch(e.target.value)}
+          onChange={(e) => props.setSearch(e.target.value)}
           type="text"
           placeholder="Pesquise por filmes"
         />
 
-        <img onClick={() => searchMovies(props.search)} src={props.Lupa} alt="" />
+        <img onClick={() => props.searchMovies(props.search)} src={props.Lupa} alt="" />
       </div>
     </div>
   );
